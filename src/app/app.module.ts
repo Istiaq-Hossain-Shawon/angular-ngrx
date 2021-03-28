69fc30127f0612b5661113f6aea11a70c76a4b36
Istiaq-hossain-shawon <hossain.ict@gmail.com>
2021-03-28 15:17:44 +0600

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingReducer } from './store/reducers/shopping.reducer';
+import { ItemAddComponent } from './item-add/item-add.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemAddComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot({
+      shopping:ShoppingReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
